9f02d6948f3bf96567f78e63032b2aa82af19d65
RavindraSingh12 <ravindrasinghkhichi@gmail.com>
2020-08-30 13:38:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import ansible_runner
-r = ansible_runner.run(playbook='main.yaml',private_data_dir='/userdata/theVerizonTask/')
+r = ansible_runner.run(playbook='main.yaml',private_data_dir='/Ansible-Runner/')
 # successful: 0
 print("Final status:")
 print(r.stats)
